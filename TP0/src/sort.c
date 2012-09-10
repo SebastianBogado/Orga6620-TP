5f70a014aa0fef30234ce4c4172b412408c1f3ed
Sebastián Bogado <sebastian.j.bogado@gmail.com>
2012-09-10 02:01:21 -0300

    Bosquejos comentados de algunas secciones de la función sort_file

--- TP0/src/sort.c
+++ TP0/src/sort.c
@@ -4,8 +4,26 @@
 static void sort_file(FILE* inStream, char** (*sort_func)(char**, unsigned )) {	
 	printf("Armando buffer... @todo\n");
 	printf("Ordenando... @todo\n");
+	/* suponiendo que hay una variable "lineas" con la cantidad de líneas, pLinea, el vector mismo, y que mantienen el "\n" al final, el ordenamiento es: 
+
+	sort_func(pLinea, lineas);
+	
+	*/
 	printf("Imprimiendo salida... @todo\n");
+	/* la impresión es:
+
+	for (int i = 0; i < lineas, ++i)
+		printf("%s", pLinea[i]);
+	
+	*/
 	printf("Liberando memoria del buffer... @todo\n");
+	/* la liberación, que podría hacerse después de imprimir cada línea en el for de arriba, es:
+
+	for (int i = 0; i < lineas, ++i)
+		free(pLinea[i]);
+
+	free(pLinea);
+	*/	
 }
 
 void sort(unsigned n, char* files[], char** (*sort_func)(char** , unsigned )) {
