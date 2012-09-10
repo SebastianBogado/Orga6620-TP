086f5b72c2f060aa244d4aeb52cb4083d82b76a7
Sebastián Bogado <sebastian.j.bogado@gmail.com>
2012-09-09 22:06:39 -0300

    Agrego los archivos que olvidé

---
+++ TP0/src/sort.c
@@ -0,0 +1,23 @@
+#include "./sort.h"
+
+static void sort_file(FILE* inStream, char** (*sort_func)(char**, unsigned )) {	
+	printf("Armando buffer... @todo");
+	printf("Ordenando... @todo");
+	printf("Imprimiendo salida... @todo");
+	printf("Liberando memoria del buffer... @todo");
+}
+
+void sort(unsigned n, char* files[], char** (*sort_func)(char** , unsigned )) {
+	if (n) {
+		FILE* inStream;
+		for (unsigned i = 0; i < n; ++i) {
+			inStream = fopen(files[i], "rb");
+			if (inStream) {
+				sort_file(inStream, sort_func);
+				fclose(inStream);
+			} else
+				printf("error %s @todo completar con perror\n", files[i]);
+		}
+	} else
+		sort_file(stdin, sort_func);
+}
---
+++ TP0/src/sort.h
@@ -0,0 +1,14 @@
+#ifndef __SORT__
+#define __SORT__
+
+/**
+ * Ordena las líneas de los archivos en orden ascendente e imprime por pantalla.
+ * @param n la cantidad de archivos
+ * @param files los archivos
+ * @param sort_func puntero a la función de ordenamiento
+ *
+ * Si n == 0, se toma como entrada stdin.
+ */
+void sort(unsigned n, char* files[], char** (*sort_func)(char** , unsigned ));
+
+#endif  // __SORT__
