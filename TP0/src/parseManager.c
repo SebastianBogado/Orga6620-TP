22b6b95f5d56c3155060bfc350e61780ab14c71f
AleMarra <alemarra@alemarra-DeskLinux.(none)>
2012-09-30 20:03:41 -0300

    Fixed, se agrega un unico byte por \n cuando no termina. Probado con pdf, png y texto

--- TP0/src/parseManager.c
+++ TP0/src/parseManager.c
@@ -7,41 +7,21 @@ unsigned cargarBuffer(char* *buffer, FILE* stream);
 unsigned parseLineas(line** *pLinea, unsigned lineas, FILE* stream){
 
 	char* buffer = NULL;
-//	int quedanLineas;
 	unsigned bufferLen;
-
-	/*
-	while (!feof(stream)) {
-	quedanLineas = cargarBuffer(&buffer, stream);
-
-		// si no quedan mas lineas, y la ultima leída es valida, la escribe
-		if (quedanLineas ||
-		    (strcmp(buffer, (*pLinea)[lineas-1]) != 0)) {
-
-			bufferLen = strlen(buffer);
-
-			++lineas;
-			// reserva espacio para una nueva linea
-			(*pLinea) = (char**)realloc((*pLinea), lineas * sizeof(char*));
-			(*pLinea)[lineas-1] = NULL;
-			// reserva espacio para almacenar la linea guardada en el buffer
-			(*pLinea)[lineas-1] = (char*)realloc((*pLinea)[lineas-1], bufferLen+1);
-
-			strcpy((*pLinea)[lineas-1], buffer);
-		}
-	*/
+	unsigned lineSize = 0;
 
 	bufferLen = cargarBuffer(&buffer, stream);
 
-	unsigned lineSize = 0;
-	for (int i=0; i<bufferLen; i++){
-		lineSize++;
-		if (buffer[i] == '\n'){
-			char* lineBuff = (char*) malloc(lineSize*sizeof(char));
+	for (int i=0; i<=bufferLen; i++){
 
-			++lineas;
+		lineSize++;
+
+		if (buffer[i] == '\n') {
+
+			char* lineBuff = (char*) malloc(lineSize*sizeof(char));
 			memcpy(lineBuff, buffer + i + 1 - lineSize, lineSize);
 
+			++lineas;
 			(*pLinea) = (line**)realloc((*pLinea), lineas * sizeof(char*));
 			(*pLinea)[lineas-1] = createLine(lineBuff, lineSize);
 
@@ -50,54 +30,11 @@ unsigned parseLineas(line** *pLinea, unsigned lineas, FILE* stream){
 	}
 
 	free(buffer);
-
 	return lineas;
 }
 
 
 unsigned cargarBuffer(char* *buffer, FILE* stream){
-/*
-	const int len_buffer = 1024;
-	const int tam_buffer = len_buffer +1;
-
-
-	// la siguiente variable cuenta la cantidad de veces que fue necesario
-	// realocar el buffer + 1 (entonces la capacidad es bufferInc*len_buffer+1)
-	unsigned bufferInc = 0;
-	unsigned bufferCapac;
-	unsigned bufferLen;
-	char* resultadoFGetS;
-
-	do {
-
-		// prepara el buffer para una nueva lectura, ampliando su capacidad
-		bufferInc++;
-		bufferCapac = bufferInc*len_buffer + 1;
-		(*buffer) = (char*)realloc((*buffer), bufferCapac*sizeof(char));
-		// buffer + bufferCapac - (tam_buffer) pone al puntero en la
-		// posición inicial de la porción nueva de memoria
-
-		resultadoFGetS = fgets((*buffer) + bufferCapac - (tam_buffer),
-							  tam_buffer, stream);
-		bufferLen = strlen(*buffer);
-		// Mientras que fgets no devuelva un puntero nulo y
-		// que el último char no sea un fin de línea, repetir
-
-	}while (resultadoFGetS &&
-			((*buffer)[bufferLen-1] != '\n'));
-
-	if (!resultadoFGetS){
-		// si el archivo no termina con un salto de linea
-		// se lo agrega para normalizar todas las lineas
-		if((*buffer)[bufferLen-1] != '\n'){
-			(*buffer)[bufferLen] = '\n';
-
-			(*buffer)[bufferLen+1] = '\0';
-		}
-		//Indica fin del archivo, no hay mas lineas
-		bufferLen = 0;
-	}
-*/
 
 	int bufferSize = 1024;
 	unsigned bufferInc = 0;
@@ -112,8 +49,7 @@ unsigned cargarBuffer(char* *buffer, FILE* stream){
 		(*buffer) = (char*)realloc((*buffer), bufferCap*sizeof(char));
 		readBytes = (unsigned)fread(
 				((*buffer) + bufferCap - (bufferSize +1)),
-				 1,
-				 (bufferSize +1),
+				 1, (bufferSize +1),
 				 stream);
 
 		bufferLen += readBytes;
@@ -128,8 +64,5 @@ unsigned cargarBuffer(char* *buffer, FILE* stream){
 			(*buffer)[bufferLen+1] = '\0';
 		}
 	}
-
 	return bufferLen;
 }
-
-
---
+++ TP0/src/sort.t
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+				fclose(inStream);
+				lineas =+ parseLineas(&pLinea, lineas, inStream);
+				perror(files[i]);
+			if (inStream) {
+			inStream = fopen(files[i], "rb");
+			} else
+		FILE* inStream;
+		for (unsigned i = 0; i < n; ++i) {
+		freeLine(&pLinea[i]);
+		lineas =+ parseLineas(&pLinea, lineas, stdin);
+		printLine(pLinea[i]);
+		}
+	//Liberación
+	//No hay archivos por argumento
+	//print salida
+	for (int i = 0; i < lineas; ++i)
+	for (int i = 0; i < lineas; ++i)
+	free(pLinea);
+	if (n) {
+	line** pLinea = NULL;
+	pLinea = sort_func(pLinea, lineas);
+	unsigned lineas = 0;
+	} else
+#include "./parseManager.h"
+#include "./sort.h"
+#include <stdio.h>
+void sort(unsigned n, char* files[], line** (*sort_func)(line** , unsigned )) {
+}
