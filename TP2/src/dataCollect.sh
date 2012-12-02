0531d75ac8485260c45bd8fee4895fa574ab98a0
Sebastián Bogado <sebastian.j.bogado@gmail.com>
2012-12-02 03:21:49 -0300

    Agregado el `time` al dataCollect, y testeado el unroll de un for

--- TP2/src/dataCollect.sh
+++ TP2/src/dataCollect.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# $1 = sed script
+# $2 = nombre de aquello que se busca promedio
+promedio () {
+	acum=0
+	for val in `sed <$temp -n "$1"`
+	do
+		acum=$(echo "scale=2; $acum + $val" | bc)
+	done
+	
+	promedio=$( echo "scale=2; $acum / $corridas" | bc)
+	
+	echo "  $2: $promedio" >> $temp
+	echo -ne "$2: $promedio; " >> $outSmall
+}
+
 if [ $# -lt 2 ]; then
 	echo "dataCollect <repeticiones> [EJECUTABLE/S...]" 
 	exit 1
@@ -32,27 +47,29 @@ do
 	while [ $i -lt $corridas ] 
 	do
 		echo -n "."
-		echo " Corrida $i" >> $temp
+		echo -e " Corrida $i\n" >> $temp
 
-		valgrind --tool=cachegrind --D1=256,2,64 "./$file" WatorOut.txt 2>&1 | grep ".*D.*" | sed "s/==[0-9]*==/   /g" >> $temp
+		/usr/bin/time -pao $temp valgrind --tool=cachegrind --D1=256,2,64 "./$file" WatorOut.txt 2>&1 | grep ".*D.*" | sed "s/==[0-9]*==/   /g" >> $temp  
 		
 		echo -ne " Fin simulacion: $(tail -n 1 WatorOut.txt) \n" >> $temp
 		
 		i=$(( $i + 1 ))
 	done
-	
-	MR=0
-	for rate in `sed <$temp -n 's/\s\+D1  miss rate:\s\+\([0-9]\+\.[0-9]\+\)%.\+/\1 /g p'`
-	do
-		MR=$(echo "scale=2; $MR + $rate" | bc)
-	done
-	
-	promedio=$( echo "scale=2; $MR / $corridas" | bc)
-	
-	echo -e " Promedio: $promedio \n\n" >> $temp
-	cat $temp >> $outFull
 
-	echo -ne "$file - Promedio MR: $promedio \n\n" >> $outSmall
+	echo -e "\n Promedios:" >> $temp	
+	echo -ne "$file - Promedios:\n\t" >> $outSmall
+	
+	promedio 's/\s\+D1  miss rate:\s\+\([0-9]\+\.[0-9]\+\)%.\+/\1 /g p' 'MR'
+	echo " time -> " >> $temp
+	echo -ne "\n\ttime -> " >> $outSmall
+	promedio 's/^real \([0-9]\+\.[0-9]\+\)$/\1 /g p' 'r'
+	promedio 's/^user \([0-9]\+\.[0-9]\+\)$/\1 /g p' 'u'
+	promedio 's/^sys \([0-9]\+\.[0-9]\+\)$/\1 /g p' 's'
+
+	echo -e "\n" >> $temp
+	echo -e "\n" >> $outSmall
+
+	cat $temp >> $outFull
 done
 
 rm cachegrind*
